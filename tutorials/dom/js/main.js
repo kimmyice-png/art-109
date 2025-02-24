99c1d7f2b59d65b0c4bef45a57241162a79fc706
kimmyice-png <kimmyicey@gmail.com>
2025-02-23 18:37:59 -0800

    Delete Burnout.mp4

---
+++ tutorials/dom/js/main.js
@@ -0,0 +1,26 @@
+let image0 = document.querySelector("#image-0")
+let image1 = document.querySelector("#image-1")
+let image2 = document.querySelector("#image-2")
+let image3 = document.querySelector("#image-3")
+
+
+
+image0.addEventListener("click", function(){
+    image0.style.display = "none"
+    alert("hello there")
+})
+
+image1.addEventListener("click", function(){
+    image1.style.display = "none"
+    alert("how is your screen")
+})
+
+image2.addEventListener("click", function(){
+    image2.style.display = "none"
+    alert("dont look behind you :)")
+})
+
+image3.addEventListener("click", function(){
+    image3.style.display = "none"
+    alert("turn this screen off")
+})
