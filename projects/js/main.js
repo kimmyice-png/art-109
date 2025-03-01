44adcce8fc9933adbd029911ab1cbf2dbe3eac55
kimmyice-png <kimmyicey@gmail.com>
2025-02-28 19:58:39 -0800

    art resume

--- projects/js/main.js
+++ projects/js/main.js
@@ -5,7 +5,7 @@ let click2 = document.querySelector("#happy-dragon");
 
 click1.addEventListener("click",function(){
     click1.style.display = "none"
-    alert("are you sure :(")
+    alert("are you sure? :(")
 })
 click2.addEventListener("click",function(){
     click2.style.display = "none"
